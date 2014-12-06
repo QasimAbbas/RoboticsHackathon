b430542cbee43632e5f6437f2e43e3ba27bbefd4
qasim52 <qasimabbas@nbp-146-79.nbp.ruw.rutgers.edu>
2014-12-06 12:06:10 -0500

    Initial Commit

---
+++ Decepticon-SDK-master/arduino/README.md
@@ -0,0 +1,5 @@
+<h3> Dependencies </h3>
+
+	sudo apt-get install arduino
+
+Extract NewPing/ from NewPing_v1.5.zip and copy to /usr/share/arduino/libraries/ folder
---
+++ Decepticon-SDK-master/raspi/agent.cpp
@@ -0,0 +1,58 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "decepticon/decepticon.hpp"
+
+using namespace cv;
+
+int main() {
+  Decepticon d();
+  
+  //Testing Motors
+  
+  d.claw_open();
+  
+  d.set_left(50);
+  d.set_right(50);
+  Q
+  usleep(1 * 1E3)
+  
+  usleep(1 * 1E3)
+  
+  d.set_left(50);
+  d.set_right(300);
+  
+  usleep(1 * 1E3)
+  
+  d.set_right(50);
+  d.set_left(50);
+  
+  usleep(1 * 1E3)
+  
+  d.set_right(300);
+  d.set_left(50);
+  
+  usleep(1 * 1E3)
+  
+  d.set_left(50);
+  d.set_right(50);
+  
+  usleep(1 * 1E3)
+  
+  d.set_right(300);
+  d.set_left(50);
+  
+  usleep(1 * 1E3)
+  
+  d.set_left(50);
+  d.set_left(50);
+  
+  
+  usleep(1 * 1E3)
+  
+  d.set_left(0);
+  d.set_right(0);
+  
+  
+  return 0;
+}
